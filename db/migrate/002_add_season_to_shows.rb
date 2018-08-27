d1e90390c03c4a9300d3bef2fd596d4dd792a371
Brandon Green <brandon@brandonleeproductions.com>
2018-08-27 23:00:06 +0000

    automatically backed up by learn

---
+++ db/migrate/002_add_season_to_shows.rb
@@ -0,0 +1,4 @@
+# In the db/migrate folder, create another file,
+# 002_add_season_to_shows.rb, and write a migration
+# to add a column, season, to the shows table.
+# The datatype of this column is string.
