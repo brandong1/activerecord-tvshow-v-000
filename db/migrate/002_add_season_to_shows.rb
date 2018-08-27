150b6fcdadf771d1cf9db6fcfc06765953909a29
Brandon Green <brandon@brandonleeproductions.com>
2018-08-27 23:26:58 +0000

    automatically backed up by learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -2,3 +2,8 @@
 # 002_add_season_to_shows.rb, and write a migration
 # to add a column, season, to the shows table.
 # The datatype of this column is string.
+class AddSeasonToShows < ActiveRecord::Migration
+  def change
+    add_column :shows, :season, :string
+  end
+end
